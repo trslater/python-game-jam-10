cd740816a789c727556220690fd103ef5252a742
Tristan Slater <hello@trslater.ca>
2023-09-29 15:16:18 -0700

    Update map

--- assets/tiles.tsx
+++ assets/tiles.tsx
@@ -1,29 +1,21 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Tiles" tilewidth="16" tileheight="32" tilecount="6" columns="0">
+<tileset version="1.5" tiledversion="1.7.2" name="Tiles" tilewidth="16" tileheight="32" tilecount="3" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="3">
-  <image width="16" height="16" source="grass.png"/>
- </tile>
  <tile id="8">
   <image width="16" height="32" source="crate.png"/>
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="8.125" width="16" height="23.875"/>
+   <object id="1" x="0" y="9.96065" width="16" height="22.0393"/>
    <object id="2" x="0" y="10" width="16" height="22"/>
+   <object id="4" x="0" y="10" width="16" height="22"/>
   </objectgroup>
  </tile>
- <tile id="9">
-  <image width="16" height="32" source="player.png"/>
+ <tile id="15">
+  <image width="16" height="16" source="sand.png"/>
+ </tile>
+ <tile id="16">
+  <image width="16" height="32" source="player-front.png"/>
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="25.875" width="15.625" height="5.75"/>
+   <object id="1" x="0.183565" y="24.506" width="15.6948" height="7.52617"/>
   </objectgroup>
- </tile>
- <tile id="11">
-  <image width="16" height="16" source="wall.png"/>
- </tile>
- <tile id="12">
-  <image width="16" height="32" source="door.png"/>
- </tile>
- <tile id="13">
-  <image width="16" height="16" source="stairs.png"/>
  </tile>
 </tileset>
